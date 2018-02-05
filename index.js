3b9a3686113165c7e4a97a97f42634c37db9f721
David Markel <davidgmarkel@gmail.com>
2018-02-05 23:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ function multiply(a, b) {
   return a * b;
 }
 
-funciton divide(a, b) {
+function divide(a, b) {
   return a / b;
 }
 
 function inc(n) {
   return n += 1;
 }
-
-a = Math.floor(Math.random() * 1000);
-b = Math.floor(Math.random() * 1000)
